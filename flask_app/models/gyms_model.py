1b364c7858f5a38d8fa0fd8b64e7bb8041b9b686
Antoine Pepino <111791015+FrenchieAP@users.noreply.github.com>
2023-01-03 12:55:01 -0800

    Update gyms_model.py

--- flask_app/models/gyms_model.py
+++ flask_app/models/gyms_model.py
@@ -17,14 +17,14 @@ class Gym:
     # @classmethod 
     # def searching(cls, find, near):
     #     query = f"{find} {near}"
-    #     r = requests.get(f"https://maps.googleapis.com/maps/api/place/textsearch/json?key=MyKeyGoesHere&query={query}&type=gym")
+    #     r = requests.get(f"https://maps.googleapis.com/maps/api/place/textsearch/json?key=ApiKeyGoesHere&query={query}&type=gym")
     #     result = r.json()
     #     print(result)
 
     @classmethod
     def get_results(cls, find, near):
         query = f"{find} {near}"
-        r = requests.get(f"https://maps.googleapis.com/maps/api/place/textsearch/json?key=MyKeyGoesHere&query={query}&type=gym")
+        r = requests.get(f"https://maps.googleapis.com/maps/api/place/textsearch/json?key=ApiKeyGoesHere&query={query}&type=gym")
         result = r.json()
         gym_list = []
         print(result)
@@ -32,12 +32,17 @@ class Gym:
             gym_info = {
                 "name": gym["name"],
                 "formatted_address": gym["formatted_address"],
-                "photos": f'https://maps.googleapis.com/maps/api/place/photo?photoreference={gym["photos"][0]["photo_reference"]}&sensor=false&maxheight=200&maxwidth=200&key=MyKeyGoesHere',
+                "photos" : "No photos",
                 "rating" : gym["rating"],
                 # "url" : gym["url"]
                 "place_id": gym["place_id"]
             }
-            print(gym_info["photos"])
+            try: 
+                gym_info["photos"] = f'https://maps.googleapis.com/maps/api/place/photo?photoreference={gym["photos"][0]["photo_reference"]}&sensor=false&maxheight=200&maxwidth=300&key=ApiKeyGoesHere'
+
+            except: 
+                gym_info["photos"] = "https://i.imgur.com/NO25iZV.png"
+            # print(gym_info["photos"])
             this_gym = cls(gym_info)
             gym_list.append(this_gym)
         return gym_list
