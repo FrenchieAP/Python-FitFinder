e75a8e46c489d34f3b8e09455d393b6e041d908c
Frenchie AP <anthony.pepinoi1@gmail.com>
2023-04-06 15:12:14 -0700

    updated config file

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from config import connectToMySQL
+
 app = Flask(__name__)
 app.secret_key = "shhhhhh"
-DATABASE = "fit_finder_schema"
+DATABASE = "heroku_1310f620a16f672"
+mysql = connectToMySQL(DATABASE)
 
 from flask_app.controllers import users_controller
 import logging
