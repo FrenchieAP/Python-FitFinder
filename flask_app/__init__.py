c0c5f3ab7070ca38af4939bf09f869f54ba828b7
frenchieap <antoinevpepino@gmail.com>
2023-12-03 16:38:17 -0800

    app optimized for mac

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -3,7 +3,7 @@ from flask_app.config.mysqlconnection import connectToMySQL
 
 app = Flask(__name__)
 app.secret_key = "shhhhhh"
-DATABASE = "fit_finder_schema"
+DATABASE = "fitfinder"
 # DATABASE = "heroku_1310f620a16f672"
 mysql = connectToMySQL(DATABASE)
 
